e568e6a6a9152a582c3507915663f46ff5caec0c
藤原　涼 <megadreams14@gmail.com>
2012-11-21 02:00:27 +0900

    テストケース追加

---
+++ fuel/app/tests/controller/test_test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Unit Tests for Modules
+ *
+ * @group App
+ */
+
+class Test_Testclassname extends TestCase {
+    // テスメソッド
+    public function test_exsample() {
+         $this->assertEquals(2,2);
+    } 
+}
+
+
